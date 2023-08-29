f061e91c30289b8bccdec115358c75605621f17f
Ika Purwanti <ikapurwanti144@gmail.com>
2023-08-29 22:09:20 +0700

    Finishing feature profile

---
+++ latihan1Git/src/main/kotlin/Profile.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var name = "Ika Purwanti"
+    var age = 20
+    var campus = "Universitas Amikom Yogyakarta"
+    println("--------------------------------------------")
+    println("Welcome to My Profile")
+    println("--------------------------------------------")
+    println("Name\t:$name")
+    println("Age\t:$age")
+    println("University\t:$campus")
+}
